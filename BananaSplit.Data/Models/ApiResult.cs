c8776ae92faa804a9152bba77dd11edd379e5e69
Charles Taylor <charles@netatvantage.com>
2013-01-08 19:58:54 -0800

    new files

---
+++ BananaSplit.Data/Models/ApiResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BananaSplit.Data.Models
+{
+    public class ApiResult
+    {
+        public Boolean Success { get; set; }
+
+        public String Description { get; set; }
+    }
+}
---
+++ BananaSplit.Data/Models/ApiResultCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BananaSplit.Data.Models
+{
+    public class ApiResultCollection<T> where T : class //where C: class
+    {
+        public Boolean Success { get; set; }
+
+        public String Description { get; set; }
+
+        public virtual List<T> ApiCollection { get; set; }
+    }
+}
