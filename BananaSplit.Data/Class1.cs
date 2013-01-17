f0cebb8a0845b10486cce34feb482bfaae327bb7
Charles Taylor <charles@netatvantage.com>
2013-01-16 18:39:22 -0800

    Updated

---
+++ BananaSplit.Data/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BananaSplit.Data
+{
+    public class Class1
+    {
+    }
+}
+
+
+
+/*
+
+Member
+MemberType
+Partner - LastAccessed, DateCreated, DateUpdated, IsArchived
+Invoice
+Team
+Tickets
+ 
+Partners added by member are not true partners until they download the
+app and register through Facebook. Once they do, then put them in the
+partner table and member table and asscociate them with the member.
+ 
+Don't skip mobile number registration when added someone manually and
+when registering new user.
+ 
+Allow user to request adding a new team if the one they want isn't
+available.
+ 
+Get Google Analytics account info
+ 
+Does Paypal have a programmatic way to find out credit card fees?
+ 
+allow non managing partner to stop invoice alerts by clicking a button
+saying they've paid it.
+ 
+PostSeason button won't show until it is setup on the backend.
+ 
+When PostSeason is setup on the backend, send push notification to all
+people that have season tickets for that team.
+ 
+App
+
+ 
+Hospitality House Be there by 3pm.
+714-542-9576
+ 
+ 
+Spin
+714-751-1101 mon-fri 8:30am - 5pm
+ 
+victory
+714-245-2580
+ 
+ 
+Unidos
+714-351-4624
+
+
+*/
